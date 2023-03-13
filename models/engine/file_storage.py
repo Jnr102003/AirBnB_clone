eb17741641257c81dcf8893cdbb6ee967a5b322d
Jnr102003 <52883414+Jnr102003@users.noreply.github.com>
2023-03-13 11:08:00 +0000

    Create file_storage.py

---
+++ models/engine/file_storage.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""file storage module"""
+
+
+import json
+import os
+
+
+class FileStorage:
+    """a class that serializes and deserializes instances
+    it inherits from the BaseModel class
+    Attributes:
+        __file_path: name of path to save the objects
+        __objects: an empty dictionary that stores the names
+        of all objects of the class
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """a method that returns the dictionary of objects"""
+        return self.__objects
+
+    def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id
+        """
+        if obj is not None:
+            key = '{}.{}'.format(type(obj).__name__, obj.id)
+            self.__objects[key] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file __file_path"""
+        odict = self.__objects
+        objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(objdict, f)
+
+    def reload(self):
+        """deserializes json file to __objects"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
+        dct = {'BaseModel': BaseModel, 'User': User, 'Place': Place,
+               'City': City, 'Amenity': Amenity, 'State': State,
+               'Review': Review}
+
+        if os.path.exists(FileStorage.__file_path) is True:
+            with open(FileStorage.__file_path, 'r') as f:
+                for key, value in json.load(f).items():
+                    self.new(dct[value['__class__']](**value))
